7d8093205c85b6312c3273b351cab617d42684c5
Moses Finimento <mosesfinimento@googlemail.com>
2024-07-24 15:06:01 +0200

    basics, Werte und Parameter

---
+++ Flutter_Dart/Dart/randomDatas/wert_und_typ_zuweisung.dart
@@ -0,0 +1,4 @@
+const a = "const muss initialisiert werden"; //Wert und Typ sind unveränderbar
+final b; //Wert und Typ sind unveränderbar
+var c; //Wert ist veränderbar
+dynamic d; //Typ ist veränderbar
